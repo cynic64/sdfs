81ad6240bfc6daed285045ae30ac034d23515c18
cynic64 <f3bl@gmx.com>
2023-03-24 15:43:04 +0100

    Color-code debug reaction lines

--- shaders/debug.fs.glsl
+++ shaders/debug.fs.glsl
@@ -1,7 +1,9 @@
 #version 450
 
+layout (location = 0) in vec3 in_dir;
+
 layout (location = 0) out vec4 out_color;
 
 void main() {
-	out_color = vec4(1, 0, 1, 1);
+	out_color = vec4(normalize(in_dir) * 0.5 + 0.5, 1);
 }
--- shaders/debug.vs.glsl
+++ shaders/debug.vs.glsl
@@ -20,9 +20,11 @@ layout(std140, binding = 0) buffer Debug {
 	vec4 line_dirs[DEBUG_MAX_LINES];
 } data;
 
+layout (location = 0) out vec3 dir;
+
 void main() {
 	vec3 pos = data.line_poss[gl_InstanceIndex].xyz;
-	vec3 dir = data.line_dirs[gl_InstanceIndex].xyz;
+	dir = data.line_dirs[gl_InstanceIndex].xyz;
 	vec3 vertices[] = vec3[](pos, pos + dir);
 	vec3 pos_worldspace = vertices[gl_VertexIndex];
 
